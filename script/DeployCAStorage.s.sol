0a908aaf70d9abfdaedeb4b72a6dd05e93b271be
Paolo Moser <paolo.moser2199@gmail.com>
2024-11-05 19:06:36 +0100

    add logs

--- script/DeployCAStorage.s.sol
+++ script/DeployCAStorage.s.sol
@@ -1,25 +1,24 @@
 pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import "forge-std/console2.sol";
 import {CA_Storage} from "../contracts/CA_Storage.sol";
 
 contract DeployCAStorage is Script {
 
     function run() external {
-        // Determina l'indirizzo del deployer
+
+        uint256 chainId = block.chainid;
+        console2.log("You are deploying on ChainID %d", chainId);
+
+        // Get deployer address
         uint256 deployerKey = uint256(vm.envOr("STORAGE_OWNER_KEY", bytes32(0)));
         vm.startBroadcast(deployerKey);
 
         // Deploy del contratto CA_Storage
         CA_Storage caStorage = new CA_Storage();
-        //console2.log("Deployed CA_Storage to", address(caStorage));
+        console2.log("Deployed CA_Storage to", address(caStorage));
 
-
-        /*[] memory CA_key_array = new bytes[](1);
-
-        CA_key_array[0] = myCAkey;
-
-        caStorage.addPublicKeys(CA_key_array);*/
 
         vm.stopBroadcast();
     }
